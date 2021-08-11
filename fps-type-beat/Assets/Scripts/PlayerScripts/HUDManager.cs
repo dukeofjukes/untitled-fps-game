7b32e0786020d936658fe3aa7e0c9a0bc9978f49
Brandon Burtchell <brandonburtchell@gmail.com>
2021-08-10 20:32:13 -0500

    fixed hitmark
    need to solve parallel coroutines problem, want to keep hitmark 100% visible on multiple hits

--- fps-type-beat/Assets/Scripts/PlayerScripts/HUDManager.cs
+++ fps-type-beat/Assets/Scripts/PlayerScripts/HUDManager.cs
@@ -6,15 +6,22 @@ using UnityEngine.UI;
 public class HUDManager : MonoBehaviour {
   [Header ("HUD Object References")]
   public Image hitmark;
-  public float hitmarkVisibleDuration = 1f;
+  public float hitmarkVisibleDuration = 0.3f;
+  public float hitmarkFadeAwayDuration = 0.3f;
 
   private void Start() {
-    hitmark.enabled = false;
+    hitmark.color = new Color(1, 1, 1, 0);
   }
 
   public IEnumerator showHitmark() {
-    hitmark.enabled = true;
-    yield return hitmarkVisibleDuration;
-    hitmark.enabled = false;
+    hitmark.color = new Color(1, 1, 1, 1);
+    //FIXME: figure out how to allow interruptions of the same coroutine, since i want the hitmark to persist on multiple shots
+
+    yield return new WaitForSeconds(hitmarkVisibleDuration);
+
+    for (float i = 1; i >= 0; i -= Time.deltaTime / hitmarkFadeAwayDuration) {
+      hitmark.color = new Color(1, 1, 1, i);
+      yield return null;
+    }
   }
 }
