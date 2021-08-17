f068d23e4506193fd00db0b197b312a8b71d5fe1
Brandon Burtchell <brandonburtchell@gmail.com>
2021-08-17 17:46:54 -0500

    created Singleton AudioManager, added player death sound and other sound assets

--- fps-type-beat/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ fps-type-beat/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour {
   public int maxHealth = 100;
   public int health;
+  public AudioSource hitAudio;
+  //public AudioSource deathAudio;
 
   void Start() {
     health = maxHealth; // may need to change this depending on how level-loading system works, we'll want to remember the health across levels
@@ -12,12 +14,11 @@ public class PlayerHealth : MonoBehaviour {
   
   public void Damage(int damageAmount = 5) {
     health -= damageAmount;
+    hitAudio.Play();
 
-    // TODO: gameover state if health <= 0
-    /*
     if (health <= 0) {
-      // game over
+      FindObjectOfType<AudioManager>().Play("PlayerDeath");
+      // TODO: change game state to game over screen or whatever
     }
-    */
   }
 }
