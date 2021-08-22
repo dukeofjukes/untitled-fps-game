0e013ecda6e295152a53b13eadcf211c86e643dd
Brandon Burtchell <brandonburtchell@gmail.com>
2021-08-22 14:53:22 -0500

    implemented enemy health indicator
    color darkens as HP decreases (may consider another color change in the future)

--- fps-type-beat/Assets/Scripts/Enemy/EnemyHealth.cs
+++ fps-type-beat/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -8,14 +8,27 @@ public class EnemyHealth : MonoBehaviour {
   public EnemyManager enemyManager;
   private AudioManager audioManager;
 
+  public Renderer modelRenderer;
+  private Material material;
+  private float originalR, originalG, originalB;
+
   void Start() {
     audioManager = FindObjectOfType<AudioManager>();
     health = maxHealth; // may need to change this depending on how level-loading system works, we'll want to remember the health across levels
     enemyManager = gameObject.GetComponent<EnemyManager>();
+    material = modelRenderer.material;
+    originalR = material.color.r;
+    originalG = material.color.g;
+    originalB = material.color.b;
   }
   
   public void Damage(int damageAmount = 5) {
     health -= damageAmount;
+    float colorDamageMultiplier = damageAmount / (float)maxHealth;
+    float damagedR = material.color.r - (colorDamageMultiplier * originalR);
+    float damagedG = material.color.g - (colorDamageMultiplier * originalG);
+    float damagedB = material.color.b - (colorDamageMultiplier * originalB);
+    material.color = new Color(damagedR, damagedG, damagedB, material.color.a);
 
     // wake up enemy, set to AttackState:
     if (enemyManager.currentState is PatrolState) {
